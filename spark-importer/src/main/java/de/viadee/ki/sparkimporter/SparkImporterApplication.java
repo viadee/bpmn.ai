a532cad4187f27e45999ac51ea6029b83d6a9752
Frank Köhne <frank.koehne@viadee.de>
2018-08-14 14:25:01 +0200

    Error handling via logger and translated error msg.

--- spark-importer/src/main/java/de/viadee/ki/sparkimporter/SparkImporterApplication.java
+++ spark-importer/src/main/java/de/viadee/ki/sparkimporter/SparkImporterApplication.java
@@ -1,7 +1,17 @@
 package de.viadee.ki.sparkimporter;
 
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterException;
+
 import de.viadee.ki.sparkimporter.exceptions.NoDataImporterDefinedException;
 import de.viadee.ki.sparkimporter.importing.DataImportRunner;
 import de.viadee.ki.sparkimporter.importing.implementations.CSVDataImporter;
@@ -13,99 +23,89 @@ import de.viadee.ki.sparkimporter.preprocessing.steps.GetVariablesTypesOccurence
 import de.viadee.ki.sparkimporter.preprocessing.steps.VariablesTypeEscalationStep;
 import de.viadee.ki.sparkimporter.util.SparkImporterArguments;
 import de.viadee.ki.sparkimporter.util.SparkImporterUtils;
-import org.apache.commons.io.FileUtils;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.File;
 
 public class SparkImporterApplication {
 
-    private static final Logger LOG = LoggerFactory.getLogger(SparkImporterApplication.class);
-    public static SparkImporterArguments ARGS;
+	private static final Logger LOG = LoggerFactory.getLogger(SparkImporterApplication.class);
+	public static SparkImporterArguments ARGS;
 
-    public static void main(String[] arguments){
+	public static void main(String[] arguments) {
 
-        long startMillis = System.currentTimeMillis();
+		final long startMillis = System.currentTimeMillis();
 
-        ARGS = SparkImporterArguments.getInstance();
+		ARGS = SparkImporterArguments.getInstance();
 
-        //instantiate JCommander
-        // Use JCommander for flexible usage of Parameters
-        JCommander jCommander = JCommander.newBuilder()
-                .addObject(SparkImporterArguments.getInstance())
-                .build();
-        try {
-            jCommander.parse(arguments);
-        } catch (ParameterException e) {
-            LOG.error("Parsing of parameters failed. Error message: " + e.getMessage());
-            jCommander.usage();
-            System.exit(1);
-        }
+		// instantiate JCommander
+		// Use JCommander for flexible usage of Parameters
+		final JCommander jCommander = JCommander.newBuilder().addObject(SparkImporterArguments.getInstance()).build();
+		try {
+			jCommander.parse(arguments);
+		} catch (final ParameterException e) {
+			LOG.error("Parsing of parameters failed. Error message: " + e.getMessage());
+			jCommander.usage();
+			System.exit(1);
+		}
 
-        //SparkImporter code starts here
+		// SparkImporter code starts here
 
-        //Delete destination files, required to avoid exception during runtime
-        FileUtils.deleteQuietly(new File(ARGS.getFileDestination()));
+		// Delete destination files, required to avoid exception during runtime
+		FileUtils.deleteQuietly(new File(ARGS.getFileDestination()));
 
-        //Configuration is being loaded from Environment (e.g. when using spark-submit)
-        SparkSession sparkSession = SparkSession.builder()
-                .getOrCreate();
+		// Configuration is being loaded from Environment (e.g. when using spark-submit)
+		final SparkSession sparkSession = SparkSession.builder().getOrCreate();
 
-        //register our own aggregation function
-        sparkSession.udf().register("AllButEmptyString", new AllButEmptyStringAggregationFunction());
+		// register our own aggregation function
+		sparkSession.udf().register("AllButEmptyString", new AllButEmptyStringAggregationFunction());
 
-        //Import data
-        DataImportRunner dataImportRunner = DataImportRunner.getInstance();
+		// Import data
+		final DataImportRunner dataImportRunner = DataImportRunner.getInstance();
 
-        //Import from CSV file
-        dataImportRunner.setDataImporter(new CSVDataImporter());
-        Dataset<Row> dataset = null;
-        try {
-            dataset = dataImportRunner.runImport(sparkSession);
-        } catch (NoDataImporterDefinedException e) {
-            e.printStackTrace();
-        }
+		// Import from CSV file
+		dataImportRunner.setDataImporter(new CSVDataImporter());
+		Dataset<Row> dataset = null;
+		try {
+			dataset = dataImportRunner.runImport(sparkSession);
+		} catch (final NoDataImporterDefinedException e) {
+			LOG.error("Could not import data.", e);
+			System.exit(1);
+		}
 
-        //write imported CSV structure to file for debugging
-        if(SparkImporterArguments.getInstance().isWriteStepResultsToCSV()) {
-            SparkImporterUtils.getInstance().writeDatasetToCSV(dataset, "import_result");
-        }
+		// write imported CSV structure to file for debugging
+		if (SparkImporterArguments.getInstance().isWriteStepResultsToCSV()) {
+			SparkImporterUtils.getInstance().writeDatasetToCSV(dataset, "import_result");
+		}
 
+		// remove duplicated columns created at CSV import step
+		dataset = SparkImporterUtils.getInstance().removeDuplicatedColumnsFromCSV(dataset);
+		dataset = SparkImporterUtils.getInstance().removeEmptyLinesAfterImport(dataset);
 
-        //remove duplicated columns created at CSV import step
-        dataset = SparkImporterUtils.getInstance().removeDuplicatedColumnsFromCSV(dataset);
-        dataset = SparkImporterUtils.getInstance().removeEmptyLinesAfterImport(dataset);
+		// write imported unique column CSV structure to file for debugging
+		if (SparkImporterArguments.getInstance().isWriteStepResultsToCSV()) {
+			SparkImporterUtils.getInstance().writeDatasetToCSV(dataset, "import_unique_columns_result");
+		}
 
-        //write imported unique column CSV structure to file for debugging
-        if(SparkImporterArguments.getInstance().isWriteStepResultsToCSV()) {
-            SparkImporterUtils.getInstance().writeDatasetToCSV(dataset, "import_unique_columns_result");
-        }
+		// Define preprocessing steps to run
+		final PreprocessingRunner preprocessingRunner = PreprocessingRunner.getInstance();
 
-        //Define preprocessing steps to run
-        PreprocessingRunner preprocessingRunner = PreprocessingRunner.getInstance();
+		// it's faster if we do not reduce the dataset columns in the beginning and
+		// rejoin the dataset later, left steps in commented if required later
+		// preprocessingRunner.addPreprocessorStep(new ReduceColumnsDatasetStep());
+		preprocessingRunner.addPreprocessorStep(new GetVariablesTypesOccurenceStep());
+		preprocessingRunner.addPreprocessorStep(new VariablesTypeEscalationStep());
+		preprocessingRunner.addPreprocessorStep(new AddVariablesColumnsStep());
+		preprocessingRunner.addPreprocessorStep(new AggregateToProcessInstanceaStep());
+		// preprocessingRunner.addPreprocessorStep(new
+		// AddRemovedColumnsToDatasetStep());
 
-        //it's faster if we do not reduce the dataset columns in the beginning and rejoin the dataset later, left steps in commented if required later
-        //preprocessingRunner.addPreprocessorStep(new ReduceColumnsDatasetStep());
-        preprocessingRunner.addPreprocessorStep(new GetVariablesTypesOccurenceStep());
-        preprocessingRunner.addPreprocessorStep(new VariablesTypeEscalationStep());
-        preprocessingRunner.addPreprocessorStep(new AddVariablesColumnsStep());
-        preprocessingRunner.addPreprocessorStep(new AggregateToProcessInstanceaStep());
-        //preprocessingRunner.addPreprocessorStep(new AddRemovedColumnsToDatasetStep());
+		// Run preprocessing runner
+		preprocessingRunner.run(dataset, SparkImporterArguments.getInstance().isWriteStepResultsToCSV());
 
+		// Cleanup
+		sparkSession.close();
 
-        //Run preprocessing runner
-        preprocessingRunner.run(dataset, SparkImporterArguments.getInstance().isWriteStepResultsToCSV());
+		final long endMillis = System.currentTimeMillis();
 
-        //Cleanup
-        sparkSession.close();
-
-        long endMillis = System.currentTimeMillis();
-
-        LOG.info("Job ran for " + ((endMillis-startMillis)/1000) + " seconds in total.");
-    }
+		LOG.info("Job ran for " + ((endMillis - startMillis) / 1000) + " seconds in total.");
+	}
 
 }
--- spark-importer/src/main/java/de/viadee/ki/sparkimporter/util/validation/ProcessIdValidator.java
+++ spark-importer/src/main/java/de/viadee/ki/sparkimporter/util/validation/ProcessIdValidator.java
@@ -2,26 +2,25 @@ package de.viadee.ki.sparkimporter.util.validation;
 
 import com.beust.jcommander.IParameterValidator;
 import com.beust.jcommander.ParameterException;
+
 import de.viadee.ki.sparkimporter.util.SparkImporterArguments;
 
 /**
- * Validator für process-id aus {@link SparkImporterArguments} um sicherzustellen,
- * dass keine Sonderzeichen enthalten sind.
+ * Validator for process-id aus {@link SparkImporterArguments} .
  */
 public class ProcessIdValidator implements IParameterValidator {
 
-    /**
-     * Validiert dass Parameter keine Sonderzeichen enthält. Konkrekt wird überprüft, dass keine
-     * Anführungszeichen enthalten sind.
-     *
-     * @param name
-     * @param value
-     * @throws ParameterException
-     */
-    @Override
-    public void validate(String name, String value) throws ParameterException {
-        if (value.contains("'")) {
-            throw new ParameterException("Parameter " + name + " darf keine Anführungszeichen enthalten.");
-        }
-    }
+	/**
+	 * Validiert that command line parameters do not contain special characters.
+	 *
+	 * @param name
+	 * @param value
+	 * @throws ParameterException
+	 */
+	@Override
+	public void validate(String name, String value) throws ParameterException {
+		if (value.contains("'")) {
+			throw new ParameterException("Parameter " + name + " may not contain quotation marks.");
+		}
+	}
 }
