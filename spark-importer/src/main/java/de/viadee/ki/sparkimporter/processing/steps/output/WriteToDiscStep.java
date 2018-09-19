104ab955daf80120a5d6c1ea5f71e5def05cb0de
Mario Micudaj <mim@viadee.de>
2018-09-19 16:56:27 +0200

    [KI-337]  removed spaces from column names as parquet does not support them

--- spark-importer/src/main/java/de/viadee/ki/sparkimporter/processing/steps/output/WriteToDiscStep.java
+++ spark-importer/src/main/java/de/viadee/ki/sparkimporter/processing/steps/output/WriteToDiscStep.java
@@ -7,10 +7,20 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WriteToDiscStep implements PreprocessingStepInterface {
     @Override
     public Dataset<Row> runPreprocessingStep(Dataset<Row> dataset, boolean writeStepResultIntoFile, String dataLevel, Map<String, Object> parameters) {
+
+        // remove spaces from column names as parquet does not support them
+        for(String columnName : dataset.columns()) {
+            if(columnName.contains(" ")) {
+                String newColumnName = columnName.replace(' ', '_');
+                dataset = dataset.withColumnRenamed(columnName, newColumnName);
+            }
+        }
 
         SparkImporterUtils.getInstance().writeDatasetToParquet(dataset, "result");
 
