2fcb3d4ef32582fc48ee90e7894d1a6577cb1b13
Mario Micudaj <mim@viadee.de>
2018-09-28 09:55:45 +0200

    [KI-349] optimised logger to be able change logdirectory later

--- spark-importer/src/main/java/de/viadee/ki/sparkimporter/util/SparkImporterLogger.java
+++ spark-importer/src/main/java/de/viadee/ki/sparkimporter/util/SparkImporterLogger.java
@@ -10,7 +10,7 @@ import java.util.logging.SimpleFormatter;
 
 public class SparkImporterLogger {
 
-    private Logger appLogger;
+    private static Logger appLogger;
     private static FileHandler logFileHandler = null;
 
     private static final String LOG_FILE_NAME = "spark-importer.log";
@@ -20,11 +20,19 @@ public class SparkImporterLogger {
     private static SparkImporterLogger instance;
 
     private SparkImporterLogger(){
+        setupLogger();
+    }
+
+    private static void setupLogger() {
+        File logDirectory  = new File(getLogDirectory());
+        if(!logDirectory.exists()) {
+            logDirectory.mkdir();
+        }
+        if(logFileHandler != null) {
+            logFileHandler.close();
+        }
         try {
-            File logDirectory  = new File(getLogDirectory());
-            if(!logDirectory.exists()) {
-                logDirectory.mkdir();
-            }
+
             logFileHandler = new FileHandler(getLogDirectory()+"/"+LOG_FILE_NAME);
 
             logFileHandler.setFormatter(new SimpleFormatter() {
@@ -42,10 +50,10 @@ public class SparkImporterLogger {
 
             appLogger = Logger.getLogger("de.viadee.ki.spark.importer");
             appLogger.addHandler(logFileHandler);
-
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 
     public static synchronized SparkImporterLogger getInstance(){
@@ -61,14 +69,7 @@ public class SparkImporterLogger {
 
     public static void setLogDirectory(String logDirectory) {
         SparkImporterLogger.logDirectory = logDirectory;
-        if(logFileHandler != null) {
-            logFileHandler.close();
-        }
-        try {
-            logFileHandler = new FileHandler(getLogDirectory()+"/"+LOG_FILE_NAME);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        setupLogger();
     }
 
     public void writeInfo(String message) {
