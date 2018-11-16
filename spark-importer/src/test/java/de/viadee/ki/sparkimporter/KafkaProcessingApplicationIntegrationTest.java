061a02e99bb5df632d73f57234a24ed04644d75a
Mario Micudaj <mim@viadee.de>
2018-11-16 12:58:29 +0100

    added show for test

--- spark-importer/src/test/java/de/viadee/ki/sparkimporter/KafkaProcessingApplicationIntegrationTest.java
+++ spark-importer/src/test/java/de/viadee/ki/sparkimporter/KafkaProcessingApplicationIntegrationTest.java
@@ -45,7 +45,7 @@ public class KafkaProcessingApplicationIntegrationTest {
                 .csv(DATA_PROCESSING_TEST_OUTPUT_DIRECTORY_PROCESS + "/result/csv/result.csv");
 
 
-        importedDataset.show();
+        importedDataset.show(5, false‚);
 
         //check that dataset contains 4 lines
         assertEquals(4, importedDataset.count());
