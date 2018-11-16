f649e996f233e038116e2516319bb26bfcd20791
Mario Micudaj <mim@viadee.de>
2018-11-16 14:19:25 +0100

    travis test failure analysis

--- spark-importer/src/test/java/de/viadee/ki/sparkimporter/CSVImportAndProcessingApplicationIntegrationTest.java
+++ spark-importer/src/test/java/de/viadee/ki/sparkimporter/CSVImportAndProcessingApplicationIntegrationTest.java
@@ -13,6 +13,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -149,6 +150,7 @@ public class CSVImportAndProcessingApplicationIntegrationTest {
     	// TODO - this test fails because of the order of tests
         System.out.println(Arrays.toString(firstLineValues));
         System.out.println(Arrays.toString(secondLineValues));
+        System.out.println(new Date());
         assertEquals("54A250FFFBC2D61E7D98C68BACB67572", DigestUtils.md5Hex(Arrays.toString(firstLineValues)).toUpperCase());
         assertEquals("17F1FC4994A0097990C5BA2CE44B3045", DigestUtils.md5Hex(Arrays.toString(secondLineValues)).toUpperCase());
         assertEquals("F70F6D20ED7605815A2C0E67E89626E3", DigestUtils.md5Hex(Arrays.toString(thirdLineValues)).toUpperCase());
